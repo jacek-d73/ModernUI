691bb8d0a69053029c949713972eff9263e7dbfa
Jacek Dobrowolski <jacek.dobrowolski@itm.tembo.eu>
2024-02-13 10:10:49 +0100

    Add project files.

---
+++ ModernUI/Core/ObcervableObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ModernUI.Core
+{
+    internal class ObcervableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
